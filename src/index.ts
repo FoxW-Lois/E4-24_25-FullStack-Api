9de430277f893823e416b3a62a6b46ce586a662c
FoxW-Lois <whaterfoxgaming@gmail.com>
2024-10-21 15:19:04 +0200

    Correction

--- src/index.ts
+++ src/index.ts
@@ -20,50 +20,51 @@ app.get("/", checkAuth, (req : express.Request, res : express.Response) => {
 })
 
 app.post("/signup", async (req : express.Request, res : express.Response,next: express.NextFunction) => {
-    const {email, password, confirmPassword} = req.body
+    const {email, password, confirmPassword} = req.body;
     console.log("email", email, "password", password, "confirmpassword", confirmPassword);
     if(!email || !password || (password !== confirmPassword)) {
-        next(StatusCodes.BAD_REQUEST)
-        return
+        next(StatusCodes.BAD_REQUEST);
+        return;
     }
     
     try {
         const newUser = new User({
             email,
             password : await hash(password, 12)
-        })
-        await newUser.save()
-        next(StatusCodes.CREATED)
+        });
+        await newUser.save();
+        next(StatusCodes.CREATED);
     } catch (error) {
-        next(error)
+        next(error);
     }
 })
 
 app.post("/login", async (req : express.Request, res : express.Response,next: express.NextFunction) => {
     console.log(req.body);
+    console.log(req.headers);
     
-    const {email, password} = req.body
+    const {email, password} = req.body;
     console.log("email", email, "password", password);
     try {
-        const dbUser = await User.findOne({email})
+        const dbUser = await User.findOne({email});
         console.log("dbUser", dbUser);
         
         if(!dbUser) {
             console.log("User not found");
-            res.sendStatus(StatusCodes.UNAUTHORIZED)
-            return
+            res.sendStatus(StatusCodes.UNAUTHORIZED);
+            return;
             
         }
         console.log("compare",password, dbUser.password);
         
         if(!await compare(password, dbUser.password)) {
             console.log("Wrong password");
-            res.sendStatus(StatusCodes.UNAUTHORIZED)
-            return
+            res.sendStatus(StatusCodes.UNAUTHORIZED);
+            return;
         }
-        res.sendStatus(StatusCodes.OK)
+        res.sendStatus(StatusCodes.OK);
     } catch (error) {
-        next(error)
+        next(error);
     }
 })
 
@@ -75,23 +76,23 @@ const handleErrors = (err : any, req : express.Request, res : express.Response, 
     }
     res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
 }
-app.use(handleErrors)
+app.use(handleErrors);
 
 mongoose.connect(process.env.MONGO_URI!).then(async () => {
-    console.log("Connected to MongoDB")
-    const lois = await User.findOne({email: "loispujolt@gmail.com"})
+    console.log("Connected to MongoDB");
+    const lois = await User.findOne({email: "loispujolt@gmail.com"});
     if(!lois) {
         const user = new User({
             email: "loispujolt@gmail.com",
             password: "123456"
         })
-        await user.save()
+        await user.save();
     }
     else{
-        console.log("User already exists", lois)
+        console.log("User already exists", lois);
     }
 
     app.listen(process.env.PORT, () => {
-        console.log("Server started on port", process.env.PORT)
+        console.log("Server started on port", process.env.PORT);
     })
 })
